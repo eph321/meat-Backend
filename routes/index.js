ba76fe4bbbe419fb173b559367cbe87a35f01253
Franck <linfranck.pro@gmail.com>
2021-12-09 14:17:47 +0100

    modif route add-table

--- routes/index.js
+++ routes/index.js
@@ -34,41 +34,42 @@ router.post('/sign-up', async function (req, res, next) {
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email,
-      dateofbirth:req.body.dateofbirth,
+      dateofbirth: req.body.dateofbirth,
       gender: req.body.gender,
       addresses: req.body.addresses,
       avatar: req.body.avatar,
-      phone : req.body.phone,
+      phone: req.body.phone,
       preference1: req.body.preference1,
-      preference2: req.body.preference2,  
+      preference2: req.body.preference2,
       preference3: req.body.preference3,
       description: req.body.description,
       password: hash,
       token: uid2(32),
     })
-  
-    var newUserSave = await newUser.save()};
-    console.log(newUserSave)
-  
-  res.json({result:newUserSave ? true : false, newUserSave });
+
+    var newUserSave = await newUser.save()
+  };
+  console.log(newUserSave)
+
+  res.json({ result: newUserSave ? true : false, newUserSave });
 });
 
 //ROUTE UPLOAD AVATAR
 
-router.post('/upload-avatar', async function(req, res, next) {
-  
-  var pictureName = './tmp/'+uniqid()+'.jpg';
+router.post('/upload-avatar', async function (req, res, next) {
+
+  var pictureName = './tmp/' + uniqid() + '.jpg';
   var resultCopy = await req.files.avatar.mv(pictureName);
-  if(!resultCopy) {
+  if (!resultCopy) {
     let resultCloudinary = await cloudinary.uploader.upload(pictureName);
     console.log(resultCloudinary)
-    res.json({cloud : resultCloudinary });
+    res.json({ cloud: resultCloudinary });
   } else {
-    res.json({error: resultCopy});
+    res.json({ error: resultCopy });
   }
 
   fs.unlinkSync(pictureName);
-  
+
 });
 
 //ROUTE SIGN in
@@ -90,21 +91,21 @@ router.post('/sign-in', async function (req, res, next) {
 });
 
 
-router.post('/add-buddy', async function(req,res, next){
+router.post('/add-buddy', async function (req, res, next) {
   let currentUser = await userModel.findOne({
     token: req.body.userToken,
   })
 
 
-  let  newConversation = new conversationModel({   
-    
-    conversationRequest : false,
-    conversationToken : uid2(32)
+  let newConversation = new conversationModel({
+
+    conversationRequest: false,
+    conversationToken: uid2(32)
 
   })
 
 
-    
+
 
 })
 
@@ -125,24 +126,22 @@ router.post('/add-table', async function (req, res, next) {
     age: req.body.age,
     capacity: req.body.capacity,
     budget: req.body.budget,
-    token: req.body.token
-
-
-
+    token: req.body.token,
+    planner: req.body.planner
   });
 
   var newTable = await addTable.save();
   res.json({ result: newTable ? true : false, newTable });
 });
 
-router.get('/search-table', async function(req,res,next){
+router.get('/search-table', async function (req, res, next) {
   var result = await eventModel.find();
-  res.json({result: result});
+  res.json({ result: result });
 });
 
-router.get('/search-users',async function (req,res,next){
+router.get('/search-users', async function (req, res, next) {
   var result = await userModel.find();
-  res.json({ result: result});
+  res.json({ result: result });
 })
 
 module.exports = router;
